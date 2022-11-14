558507ea54959337706e6a49374323c6f3c4f0df
Per Norin <pelle.norin.79@gmail.com>
2022-11-14 17:06:46 +0100

    initial setup and config

--- SalesApp/App.xaml.cs
+++ SalesApp/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SalesApp.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +12,26 @@ using System.Windows;
 
 namespace SalesApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
+        public static IHost? app { get; private set; } // lek4 1h
+
+        public App()
+        {
+            app = Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+                services.AddSingleton<MainWindow>();
+                services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\perno\Documents\Nackademin\2-databasteknik\inlUppgift\assignment_db.mdf;Integrated Security=True;Connect Timeout=30"));
+            }).Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e) // lek4 1:15
+        {
+            await app!.StartAsync();
+            var MainWindow = app.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
---
+++ SalesApp/Contexts/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.Contexts
+{
+    internal class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        // Här ska entities vara
+    }
+}
